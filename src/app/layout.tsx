24654763ab8d952690a7d888bcfe8cd711243ba1
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-02-03 19:39:02 +0700

    FEAT: prevent use in mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
