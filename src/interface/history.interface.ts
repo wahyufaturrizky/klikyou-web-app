e099b585e0ee47fd1aa14e004e8802e478ed541e
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-02-17 00:16:58 +0700

    FEAT: type common interface

--- src/interface/history.interface.ts
+++ src/interface/history.interface.ts
@@ -1,19 +1,3 @@
-export type FormHistoryValues = {
-  docName: string;
-  id: string;
-  status: string;
-  latestApproval: string;
-  memoId: string;
-  docNumber: string;
-  textRemarks: string;
-  numericRemarks: string;
-  tags: string[];
-  collaborators: string[];
-  file: string;
-  authorizers: string[];
-  recipients: string[];
-};
-
 export interface DataTypeActionHistory {
   id: string;
   user: string;
@@ -22,17 +6,4 @@ export interface DataTypeActionHistory {
   file: string;
   fileVerHistory: string;
   updatedAt: Date;
-}
-
-export interface DataTypeInfo {
-  id: string;
-  createBy: string;
-  createAt: Date;
-  updateBy: string;
-  updatedAt: Date;
-}
-
-interface EditModal {
-  open: boolean;
-  data?: DataTypeInfo | null;
 }
