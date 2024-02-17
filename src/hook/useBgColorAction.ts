e3d31250a32d12877bf6d4c7f136b58420954b0e
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-02-17 22:15:34 +0700

    FIX: typeof string

--- src/hook/useBgColorAction.ts
+++ src/hook/useBgColorAction.ts
@@ -1,15 +1,17 @@
 export const UseBgColorAction = (val: string) => {
-  if (val?.includes("Rejected")) {
-    return "bg-red";
-  } else if (val?.includes("Approved")) {
-    return "bg-green";
-  } else if (val?.includes("Updated")) {
-    return "bg-warn";
-  } else if (val?.includes("Uploaded")) {
-    return "bg-link";
-  } else if (val?.includes("upload")) {
-    return "bg-link";
-  } else if (val?.includes("pending")) {
-    return "bg-link";
+  if (typeof val === "string") {
+    if (val?.includes("Rejected")) {
+      return "bg-red";
+    } else if (val?.includes("Approved")) {
+      return "bg-green";
+    } else if (val?.includes("Updated")) {
+      return "bg-warn";
+    } else if (val?.includes("Uploaded")) {
+      return "bg-link";
+    } else if (val?.includes("upload")) {
+      return "bg-link";
+    } else if (val?.includes("pending")) {
+      return "bg-link";
+    }
   }
 };
--- src/hook/useBgColorStatus.ts
+++ src/hook/useBgColorStatus.ts
@@ -1,16 +1,18 @@
 export const UseBgColorStatus = (val: string) => {
-  if (
-    val?.includes("Partially approved") ||
-    val?.includes("uploaded") ||
-    val?.includes("updated") ||
-    val?.includes("Partially processed")
-  ) {
-    return "bg-link";
-  } else if (val?.includes("Waiting Approval")) {
-    return "bg-gray-dark";
-  } else if (val?.includes("Fully approved")) {
-    return "bg-green";
-  } else if (val?.includes("Fully Processed")) {
-    return "bg-primary-purple";
+  if (typeof val === "string") {
+    if (
+      val?.includes("Partially approved") ||
+      val?.includes("uploaded") ||
+      val?.includes("updated") ||
+      val?.includes("Partially processed")
+    ) {
+      return "bg-link";
+    } else if (val?.includes("Waiting Approval")) {
+      return "bg-gray-dark";
+    } else if (val?.includes("Fully approved")) {
+      return "bg-green";
+    } else if (val?.includes("Fully Processed")) {
+      return "bg-primary-purple";
+    }
   }
 };
