c04a5a5657446eeccef3e5fec1a866162bbdb2c1
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-02-19 22:11:14 +0700

    FIX: update format date utc

--- src/hook/useConvertDateFormat.ts
+++ src/hook/useConvertDateFormat.ts
@@ -1,12 +1,12 @@
 // from format "2024-02-06T04:58:23.000+00:00" to "30/06/2023 17:00" Date Format
-const UseConvertDateFormat = (inputDate: Date | string = new Date()) => {
-  const dateObj = new Date(inputDate);
+const UseConvertDateFormat = (inputDate: Date | string = new Date()): string => {
+  const dateObj = typeof inputDate === "string" ? new Date(inputDate) : inputDate;
 
-  const year = dateObj.getUTCFullYear();
-  const month = String(dateObj.getUTCMonth() + 1).padStart(2, "0");
-  const day = String(dateObj.getUTCDate()).padStart(2, "0");
-  const hours = String(dateObj.getUTCHours()).padStart(2, "0");
-  const minutes = String(dateObj.getUTCMinutes()).padStart(2, "0");
+  const year = dateObj.getFullYear();
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  const day = String(dateObj.getDate()).padStart(2, "0");
+  const hours = String(dateObj.getHours()).padStart(2, "0");
+  const minutes = String(dateObj.getMinutes()).padStart(2, "0");
 
   return `${day}/${month}/${year} ${hours}:${minutes}`;
 };
