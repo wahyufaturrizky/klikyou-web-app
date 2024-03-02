280f855014ead92b869beb0bbeacf4da068964b0
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-03-02 15:54:46 +0700

    FIX: bg red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect } from "react";
+
 // Author, Software Architect, Software Engineer, Software Developer : https://www.linkedin.com/in/wahyu-fatur-rizky
 
 export default function Home() {
